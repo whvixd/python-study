86fc78a9bc19c818f4f25a30d98349cb89d2b401
wangzhixiang <wangzhixiang_v@didiglobal.com>
2021-10-29 19:17:01 +0800

    自定义模型

--- AI/pytorch-demo/net_demo.py
+++ AI/pytorch-demo/net_demo.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from collections import OrderedDict
 
 
 # 自定义多层感知机
@@ -16,3 +17,38 @@ class MLP(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         h = self.act(self.hidden(x))
         return self.output(h)
+
+
+X = torch.randn(2, 784)
+
+
+# net = MLP()
+# print(net)
+# print(net(X))
+
+
+class SequentialClone(nn.Module):
+
+    def __init__(self, *args):
+        super(SequentialClone, self).__init__()
+        if len(args) == 1 and isinstance(args[0], OrderedDict):
+            for key, module in args[0].items():
+                self.add_module(key, module)
+        else:
+            for idx, module in enumerate(args):
+                self.add_module(str(idx), module)
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        # self._modules返回一个 OrderedDict，保证会按照成员添加时的顺序遍历成员
+        for module in self._modules.values():
+            input = module(input)
+        return input
+
+
+net = SequentialClone(
+    nn.Linear(784, 256),
+    nn.ReLU(),
+    nn.Linear(256, 10)
+)
+print(net)
+print(net(X))
