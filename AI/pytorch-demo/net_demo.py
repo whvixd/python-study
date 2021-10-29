59b1a7abd373bdab0f4b3f0c76cb0bf0d833a20e
wangzhixiang <wangzhixiang_v@didiglobal.com>
2021-10-29 16:53:21 +0800

    object内置函数

--- AI/pytorch-demo/net_demo.py
+++ AI/pytorch-demo/net_demo.py
@@ -6,11 +6,13 @@ from torch import nn
 class MLP(nn.Module):
 
     def __init__(self, **kwargs):
+        # 参数，如模型参数的访问、初始化和共享
         super(MLP, self).__init__(**kwargs)
-        self.hidden = nn.Linear(784, 256)
-        self.act = nn.ReLU()
-        self.output = nn.Linear(256, 10)
+        self.hidden = nn.Linear(784, 256)  # 隐藏层
+        self.act = nn.ReLU()  # 激活函数
+        self.output = nn.Linear(256, 10)  # 输出层
 
+    # 正向传播
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         h = self.act(self.hidden(x))
         return self.output(h)
