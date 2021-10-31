6d7bfba7d5b221b00c664f9a7d3a895981906caa
wangzhixiang <wangzhixiang_v@didiglobal.com>
2021-10-31 16:17:06 +0800

    numpy pandas

--- AI/pytorch-demo/d2lzh_pytorch_test.py
+++ AI/pytorch-demo/d2lzh_pytorch_test.py
@@ -109,6 +109,45 @@ class MyTestCase(unittest.TestCase):
         # y=x**2 y'=2x ,x=4 y'=8  链式法则求导
         print(x.grad)
 
+    def test_np(self):
+        arange_demo = np.arange(0, 1, 0.1, dtype=float)
+        print(arange_demo)
+        # 创建一位数组
+        linspace_demo = np.linspace(1, 100, num=10)
+        print(linspace_demo)
+
+        # 等比数列 10**0,10**2
+        logspace_demo = np.logspace(0, 2, 20)
+
+        # 索引
+        print(logspace_demo[1])
+
+        # 生成2X3的零矩阵
+        np.zeros((2, 3), dtype=float)
+
+        # 生成3X5的矩阵，k=0,为对角数为1
+        print(np.eye(3, 5, k=0))
+
+        # 生成 3X3矩阵，对角线为 1，3，5
+        print("1d np.diag :\n", np.diag([1, 3, 5]))
+        # 2维数组，返回对角线的值
+        print("2d np.diag:\n", np.diag([[1, 2, 3],
+                                        [1, 5, 3],
+                                        [1, 2, 7]]))
+
+        # 输入一个100个随机数组
+        print(np.random.random(100))
+
+        # 随机生成10X5的随机正态分布矩阵,ravel展平
+        randn_demo = np.random.randn(10, 5)
+        print(randn_demo.ravel())
+        print(randn_demo.flatten(order='F'))
+
+        #         矩阵的创建
+        np.mat("123;456;789")
+
+        # pandas处理表格，json相关数据结构
+
 
 if __name__ == '__main__':
     unittest.main()
