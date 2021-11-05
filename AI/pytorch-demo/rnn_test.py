90f358b98c9a5188a2bc92194162892ca89ea509
wangzhixiang <wangzhixiang_v@didiglobal.com>
2021-11-05 20:46:37 +0800

    torch RNN

--- AI/pytorch-demo/rnn_test.py
+++ AI/pytorch-demo/rnn_test.py
@@ -38,6 +38,29 @@ class MyTestCase(unittest.TestCase):
                               clipping_theta, batch_size, pred_period, pred_len,
                               prefixes)
 
+    # https://tangshusen.me/Dive-into-DL-PyTorch/#/chapter06_RNN/6.5_rnn-pytorch
+    def test_pytorch_rnn(self):
+        (corpus_indices, char_to_idx, idx_to_char, vocab_size) = d2l.load_data_jay_lyrics()
+        num_hiddens = 256
+        # rnn_layer = nn.LSTM(input_size=vocab_size, hidden_size=num_hiddens) # 已测试
+        rnn_layer = nn.RNN(input_size=vocab_size, hidden_size=num_hiddens)
+
+        num_steps = 35
+        batch_size = 2
+        state = None
+        X = torch.rand(num_steps, batch_size, vocab_size)
+        Y, state_new = rnn_layer(X, state)
+        print(Y.shape, len(state_new), state_new[0].shape)
+
+        model = RNNModel(rnn_layer, vocab_size).to(device)
+        print(predict_rnn_pytorch('分开', 10, model, vocab_size, device, idx_to_char, char_to_idx))
+
+        num_epochs, batch_size, lr, clipping_theta = 250, 32, 1e-3, 1e-2  # 注意这里的学习率设置
+        pred_period, pred_len, prefixes = 50, 50, ['分开', '不分开']
+        train_and_predict_rnn_pytorch(model, num_hiddens, vocab_size, device,
+                                      corpus_indices, idx_to_char, char_to_idx,
+                                      num_epochs, num_steps, lr, clipping_theta,
+                                      batch_size, pred_period, pred_len, prefixes)
 
 
 if __name__ == '__main__':
