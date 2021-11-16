707fe67926f35f9b544899968909e597447e04ca
wangzhixiang <wangzhixiang_v@didiglobal.com>
2021-11-16 22:10:44 +0800

    优化

--- AI/pytorch-demo/rnn_test.py
+++ AI/pytorch-demo/rnn_test.py
@@ -74,6 +74,23 @@ class MyTestCase(unittest.TestCase):
         train_and_predict_rnn(gru,get_gru_params,init_gru_state,num_hiddens,vocab_size,device,corpus_indices,idx_to_char,
                               char_to_idx,False,num_epochs,num_steps,lr,clipping_theta,batch_size,pred_period,pred_len,prefixes)
 
+    def test_simple_GRU(self):
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        (corpus_indices, char_to_idx, idx_to_char, vocab_size) = d2l.load_data_jay_lyrics()
+        num_inputs, num_hiddens, num_outputs = vocab_size, 256, vocab_size
+        gru=nn.GRU(input_size=vocab_size,hidden_size=num_hiddens)
+        model=RNNModel(gru,vocab_size).to(device)
+        num_epochs, num_steps, batch_size, lr, clipping_theta = 160, 35, 32, 1e2, 1e-2
+        pred_period, pred_len, prefixes = 40, 50, ['分开', '不分开']
+
+        train_and_predict_rnn(model,get_gru_params,init_gru_state,num_hiddens,vocab_size,device,corpus_indices,idx_to_char,
+                              char_to_idx,False,num_epochs,num_steps,lr,clipping_theta,batch_size,pred_period,pred_len,prefixes)
+
+    def test_data_iter_consecutive(self):
+        my_seq = list(range(100))
+        for X, Y in data_iter_consecutive(my_seq, batch_size=2, num_steps=6):
+            print('X: ', X, '\nY:', Y, '\n')
+
 
 if __name__ == '__main__':
     unittest.main()
--- AI/util/plt_demo.py
+++ AI/util/plt_demo.py
@@ -38,6 +38,59 @@ class MyTestCase(unittest.TestCase):
         plt.bar([1, 2, 3, 4, 5], 0.2)
         plt.show()
 
+    def test_save(self):
+        years = [year for year in range(2000, 2016)]
+
+        values = [12914, 11826, 12997, 12306.41, 12327.28, 11406, 10608, 8378, 8667.02, 8052.78, 6922.52, 5744, 4196,
+                  4336, 4588, 4751]
+
+        plt.bar(years, values[::-1], color='#800080')  # 绘制柱状图，alpha 表示颜色的透明程度
+        plt.yticks([key for key in range(4000, 14000, 1000)])
+        plt.xticks(years, rotation=45)
+        plt.axis(ymin=4000, ymax=14000)
+        plt.show()  # 展示图像
+
+    def test_bar1(self):
+        xstring = '2015 2014 2013 2012 2011     \
+                   2010 2009 2008 2007 2006     \
+                   2005 2004 2003 2002 2001    2000'  # x轴标签
+        n = 6
+        ystring = [''] * n  # y轴对应的6组数据
+        ystring[
+            0] = '6793    6324    6237    5790.99    5357.1    5032    4681    3800    3863.9    3366.79    3167.66    2778    2359    2250    2170    2112'
+        ystring[
+            1] = '6473    5933    5850    5429.93    4993.17    4725    4459    3576    3645.18    3119.25    2936.96    2608    2197    2092    2017    1948'
+        ystring[
+            2] = '15157    12965    12591    11460.19    10993.92    10934    9662    7801    7471.25    6584.93    5833.95    5576    4145    4154    4348    4288'
+        ystring[
+            3] = '12914    11826    12997    12306.41    12327.28    11406    10608    8378    8667.02    8052.78    6922.52    5744    4196    4336    4588    4751'
+        ystring[
+            4] = '9566    9817    9777    9020.91    8488.21    7747    6871    5886    5773.83    5246.62    5021.75    3884    3675.14    3488.57    3273.53    3260.38'
+        ystring[
+            5] = '4845    5177    4907    4305.73    4182.11    4099    3671    3219    3351.44    3131.31    2829.35    2235    2240.74    1918.83    2033.08    1864.37'
+        labels = ['Commercial housing', 'Residential commercial housing',
+                  'high-end apartments', 'Office Building', 'Business housing', 'Others']  # 图例标签
+        colors = ['#ff7f50', '#87cefa', '#DA70D6', '#32CD32', '#6495ED', '#FF69B4']  # 指定颜色
+        #  请在此添加实现代码  #
+        # ********** Begin *********#
+        xlabels = xstring.split()  # 年份切分
+        xlabels.reverse()  # 年份序列倒序排列，从小到大
+        x = np.arange(1, n * len(xlabels), n)  # x轴条形起始位置
+        w = 0.8  # 条形宽度设置
+        for i in range(n):
+            y = ystring[i].split()
+            y.reverse()
+            y = [float(e) for e in y]  # 将划分好的字符串转为float类型
+            plt.bar(x + i * w, y, width=w, color=colors[i])  # 以指定颜色绘制柱状图
+        plt.ylim([1450, 15300])  # 指定y轴范围
+        plt.yticks(range(2000, 15000, 2000))  # 指定y轴刻度
+        plt.xlim([-1, 98])
+        plt.xticks(x + w * 2.5, xlabels, rotation=45)  # 添加x轴标签，旋转40度
+        plt.legend(labels, loc='upper left')  # 添加图例，位置为左上角
+        plt.title('Selling Prices of Six Types of Housing')
+        plt.show()  # 展示图像
+        # ********** End **********#
+
 
 if __name__ == '__main__':
     unittest.main()
