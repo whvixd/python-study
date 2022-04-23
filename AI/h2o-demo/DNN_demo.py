4984b138ecc00c00c5ce52aab7f397723b8f4222
wangzhixiang <lwangzhixiang_v@didiglobal.com>
2022-04-23 16:02:16 +0800

    h2o demo

--- AI/h2o-demo/DNN_demo.py
+++ AI/h2o-demo/DNN_demo.py
@@ -6,6 +6,8 @@ h2o 直接用 pip安装
 
 demo：
     @link https://docs.h2o.ai/h2o/latest-stable/h2o-py/docs/intro.html#installing-h2o-3
+    
+h2o暂时对CNN和RNN不支持，后续可能会将tensorflow集成进去
 '''
 
 #  this will attempt to discover an H2O at localhost:54321
@@ -42,7 +44,77 @@ def glm_demo():
     print("m_print:", m)
 
 
+def gbm_demo():
+    from h2o.estimators.gbm import H2OGradientBoostingEstimator
+
+    df[1] = df[1].asfactor()
+
+    m = H2OGradientBoostingEstimator(ntrees=10, max_depth=5)
+
+    m.train(x=df.names[2:], y='CAPSULE', training_frame=df)
+
+    print('m.type_print:', m.type)
+
+
+def dl_demo():
+    from h2o.estimators.deeplearning import H2ODeepLearningEstimator
+    df[1] = df[1].asfactor()
+
+    # 随机统一数字，每行一个
+    random = df[0].runif()
+
+    # 60%的训练集
+    train = df[random < 0.6]
+
+    # 30%的验证集
+    valid = df[0.6 <= random < 0.9]
+
+    # 10%的测试集
+    test = df[random >= 0.9]
+
+    m = H2ODeepLearningEstimator()
+
+    print('m.train_print:',m.train(x=train.names[2:],y=train.names[1],training_frame=train,validation_frame=valid))
+    print('m.train_print_end')
+
+    print('m_print:',m)
+
+    # 预测
+    print('m.predict_print:\n',m.predict(test))
+    print('m.predict_print_end')
+
+
+    # 在训练数据上显示性能
+    m.model_performance()
+
+    # 在验证数据上显示性能
+    m.model_performance(valid=True)
+
+    # 评分并计算测试数据的新指标!
+    print('m.model_performance(test_data=test)_print:',m.model_performance(test_data=test))
+    print('m.model_performance(test_data=test)_print_end')
+
+    # 训练数据的均方差
+    m.mse()
+
+    # 验证集上的均方差
+    print('m.mse_print:',m.mse(valid=True))
+
+    m.r2()
+    print('m.r2_print:',m.r2(valid=True))
+
+    print('m.confusion_matrix_print:',m.confusion_matrix())
+
+    # 混淆矩阵的最大精度
+    m.confusion_matrix(metrics="accuracy")
+
+    # check out the help for more!
+    m.confusion_matrix("min_per_class_accuracy")
+
+
 if __name__ == '__main__':
     init()
 
-    glm_demo()
+    # glm_demo()
+    # gbm_demo()
+    dl_demo()
--- test/unit_test.py
+++ test/unit_test.py
@@ -5,5 +5,15 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)  # add assertion here
 
+
+    def test_01(self):
+        l_=[1,1,3,4]
+
+        t_=l_[0<0.1]
+
+        print(t_)
+
+        self.assertEqual(True, True)  # add assertion here
+
 if __name__ == '__main__':
     unittest.main()
