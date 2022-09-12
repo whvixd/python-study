c38274621ab65b95dc4fada6166dd5ca8c939dc1
wangzhx <wangzhx@fxiaoke.com>
2022-09-12 16:54:56 +0800

    nc

--- AI/nc_io.py
+++ AI/nc_io.py
@@ -5,7 +5,7 @@ import os
 
 # panoply nc可视化工具：https://www.giss.nasa.gov/tools/panoply/download/
 
-path = "/Users/didi/Downloads/12981977/2018/NIRv.GPP.201801.v1.nc"
+path = "/Users/whvixd/Documents/individual/MODIS/dataset/gpp/1982/NIRv.GPP.198201.v1.nc"
 dst = Dataset(path, mode='r', format="netCDF4")
 
 dst.set_auto_mask(False)
@@ -36,9 +36,14 @@ print('--------------------------GPP------------------------------')
 GPP = dst.variables['GPP']
 print(GPP)
 unit = GPP.units
+scaling_factor = float(GPP.scaling_factor)
+print(scaling_factor)
 print('--------------------------GPP[i][j]------------------------------')
 for i in range(7200):
     for j in range(3600):
         val_ = GPP[i][j]
         if val_ != -9999 and val_ != 0:
-            print("lon:{:},lat:{:},val_:{:} {:}".format(lon[i], lat[j], val_, unit))
+            if -32767==val_:
+                print("-32767====lon:{:},lat:{:},val_:{:} {:}".format(lon[i], lat[j], val_*scaling_factor, unit))
+
+            print("lon:{:},lat:{:},val_:{:} {:}".format(lon[i], lat[j], val_*scaling_factor, unit))
