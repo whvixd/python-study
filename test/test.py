378e77834df511cd22c87ec4bd2b57161b639e3d
wangzhx <wangzhx@fxiaoke.com>
2023-03-04 18:24:48 +0800

    forward

--- test/test.py
+++ test/test.py
@@ -26,7 +26,40 @@ class Tom(Person, Action):
         super(Tom, self).action("running")
         super().do()
 
+    def funca(self,*args, **kwargs):
+        print(args)
+        print(kwargs)
+
+from typing import Union, Tuple, Any, Callable, Iterator, Set, Optional, overload, TypeVar, Mapping, Dict, List
+
+def _forward_unimplemented(self, *input: Any) -> None:
+    raise NotImplementedError
+
+
+
+class Model:
+
+    forward:Callable[...,Any]=_forward_unimplemented
+
+    def _call_impl(self,*args, **kwargs):
+        print(args)
+        print(kwargs)
+        self.forward(args,kwargs)
+
+
+    __call__: Callable[..., Any] = _call_impl
+
+
+class Model_C(Model):
+    def forward(self,*args, **kwargs):
+        print("Model_C")
+        print(args,kwargs)
+
 
 if __name__ == '__main__':
-    tom = Tom()
-    tom.tom_action()
+    # tom = Tom()
+    # tom.tom_action()
+    # tom.funca(1,2,3,4,a=1,b=2)
+
+    c=Model_C()
+    c(1,2,3,input=[1,2,3])
